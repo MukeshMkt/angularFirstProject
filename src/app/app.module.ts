9dd9dd45ee4b7b245d6d56179b25cddd8519283f
Mukesh <mukeshkr.thakur@cbnits.com>
2022-07-27 12:52:09 +0530

    pass data from parent to chil and child to parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
-import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComponentComponent, ParentComponent, ChildComponent],
+  declarations: [AppComponent, MyComponentComponent, ChildComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
